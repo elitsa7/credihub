fc12ddf5bb64de57473ce1f1e3532d308f1f6c99
Elitsa Dacheva <122827000+elitsa7@users.noreply.github.com>
2026-06-19 11:16:14 +0300

    refactor: move admin password to application properties

--- src/main/java/bg/credihub/config/AdminInitConfig.java
+++ src/main/java/bg/credihub/config/AdminInitConfig.java
@@ -3,13 +3,17 @@ package bg.credihub.config;
 import bg.credihub.model.entities.User;
 import bg.credihub.model.enums.Role;
 import bg.credihub.repository.UserRepository;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class AdminInitConfig {
+    @Value("${app.admin.password}")
+    private String adminPassword;
 
     @Bean
     public CommandLineRunner adminInit(UserRepository userRepository, PasswordEncoder passwordEncoder) {
@@ -22,7 +26,7 @@ public class AdminInitConfig {
                 admin.setEmail("admin@credihub.com");
                 admin.setPhoneNumber("0888888888");
                 admin.setIdentificationNumber("0000000000");
-                admin.setPassword(passwordEncoder.encode("Admin123!"));
+                admin.setPassword(passwordEncoder.encode(adminPassword));
                 admin.setRole(Role.ADMIN);
 
                 userRepository.save(admin);
