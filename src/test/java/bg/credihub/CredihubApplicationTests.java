5400087fba569d83628f69d8539bee4bc0773770
Elitsa Dacheva <122827000+elitsa7@users.noreply.github.com>
2026-07-16 09:03:25 +0300

    Configure app properties for deployment

--- src/test/java/bg/credihub/CredihubApplicationTests.java
+++ src/test/java/bg/credihub/CredihubApplicationTests.java
@@ -2,8 +2,10 @@ package bg.credihub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CredihubApplicationTests {
 
 	@Test
